f289bec5fbf6048012bcb46e40f750903f6bd60a
Andrew McDowell <madoliole@gmail.com>
2015-12-08 23:22:00 +1100

    first

---
+++ src/README.md
@@ -0,0 +1,1 @@
+This is your Chrome extension which replaces Donald Trumpt or phrase with a Donald Trumpet-arse.
---
+++ src/content.js
@@ -0,0 +1,5 @@
+(function() {
+    var text = document.body.innerHTML;
+    var newText = text.replace(/\btrump\b/ig, 'Trumpet-arse');
+    document.body.innerHTML = newText;
+})();
