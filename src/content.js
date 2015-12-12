9e1f92d3dc920ac43a053bbaa01e54bb631fba03
Andrew McDowell <madoliole@gmail.com>
2015-12-13 08:44:13 +1100

    v0.4 - fixing browsing issues

--- src/content.js
+++ src/content.js
@@ -1,5 +1,20 @@
-(function() {
-    var text = document.body.innerHTML;
-    var newText = text.replace(/\btrump\b/ig, 'Trumpet-arse');
-    document.body.innerHTML = newText;
-})();
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByTagName('*');
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        for (var j = 0; j < element.childNodes.length; j++) {
+            var node = element.childNodes[j];
+
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
+                var replacedText = text.replace(/\btrump\b/ig, 'Trumpet-arse');
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+});
